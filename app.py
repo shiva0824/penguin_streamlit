9e9f6fb000938135abf23ef59474877cdd4529b2
G.SHIVA PRASAD REDDY <73631196+shiva0824@users.noreply.github.com>
2023-02-26 23:05:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title(":red[Innomatics] Data App  :computer:")
+
+st.header("Data science Internship")
+
+st.subheader("This is :blue[Shiva Prasad] Working on My first Streamlit Application :male-office-worker:")
+
+st.text("Dataset working on: Palmer Archipelago (Antarctica) penguin data")
+
+if st.button("Click here for surprise!!"):
+   st.success("Thanks for Checking out, you made my day :)", icon="✅")
+   st.balloons()
